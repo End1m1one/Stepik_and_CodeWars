55e3ce7c62bd05db8cad300f1a4f7201a1e3c851
End1m1one <kosoruk03@mail.ru>
2022-08-18 16:38:13 +0300

    new task

---
+++ Your order, please.py
@@ -0,0 +1,12 @@
+def order(sentence):
+    sentence = sentence.split()
+    n= 1
+    res = []
+    while n != len(sentence) + 1:
+        for char in sentence:
+            for i in char:
+                if i == str(n):
+                    res.append(char)
+                    n +=1
+    return " ".join(res)
+print(order("is2 Thi1s T4est 3a"))
